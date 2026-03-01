4acad1eedfac8a61ec189388eb0bb8d06dda41f2
Aurora <18258475212m@sina.cn>
2026-03-01 12:03:02 +0800

    🎨style: 新增主题配置的相关代码，需要开发一个公共组件来切换主题（TODO）

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import 'vue-sonner/style.css'
 import { Toaster } from '@/components/ui/sonner'
 import useAppStore from '@/store/app';
 import { Button } from '@/components/ui/button'
+import { Switch } from '@/components/ui/switch'
+import { Input } from './components/ui/input';
 
 const appStore = useAppStore();
 
@@ -11,6 +13,9 @@ const appStore = useAppStore();
 <template>
   <div class="w-full h-full flex flex-col gap-10 justify-center items-center">
     <p class="text-2xl font-bold">这是一个基于shadcn-vue的vue-ts项目模板</p>
+    <p>主题色已经被改成 蓝色</p>
+    <Switch />
+    <Input placeholder="请输入" class="w-80" />
     <div class="flex justify-center items-center gap-4">
       <Button @click="appStore.handleModeChange('light')">
         切换亮色模式
